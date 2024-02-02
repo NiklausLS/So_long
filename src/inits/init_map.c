c5a26ba8be0c208cb9136d061fb1bf01303d897f
NiklausLS <nicolas.leempoels@gmail.com>
2024-02-02 10:06:06 +0100

    changing error handling

--- src/inits/init_map.c
+++ src/inits/init_map.c
@@ -6,7 +6,7 @@
 /*   By: nileempo <nileempo@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 13:36:57 by nileempo          #+#    #+#             */
-/*   Updated: 2024/02/01 23:52:59 by nileempo         ###   ########.fr       */
+/*   Updated: 2024/02/02 10:05:19 by nileempo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,17 @@ void	init_textures(t_data *data)
 {
 	data->ground = mlx_xpm_file_to_image(data->mlx_ptr, "./images/ground.xpm", &data->width, &data->height);
 	if (!data->ground)
-	{
-		ft_putstr_fd("Error\nGround texture failed to load\n", 2);
-		exit(EXIT_FAILURE);
-	}
+		ft_errorexit("Error\nGround texture failed to load\n");
 	data->wall = mlx_xpm_file_to_image(data->mlx_ptr, "./images/wall.xpm", &data->width, &data->height);
 	if (!data->wall)
-	{
-		ft_putstr_fd("Error\nWall texture failed to load\n", 2);
-		exit(EXIT_FAILURE);
-	}
+		ft_errorexit("Error\nWall texture failed to load\n");
 	data->collectible = mlx_xpm_file_to_image(data->mlx_ptr, "./images/collectible.xpm", &data->width, &data->height);
 	if (!data->collectible)
-	{
-		ft_putstr_fd("Error\nCollectible texture failed to load\n", 2);
-		exit(EXIT_FAILURE);
-	}
+		ft_errorexit("Error\nCollectible texture failed to load\n");
 	data->player = mlx_xpm_file_to_image(data->mlx_ptr, "./images/player.xpm", &data->width, &data->height);
 	if (!data->player)
-	{
-		ft_putstr_fd("Error\nCharacter texture failed to load\n", 2);
-		exit(EXIT_FAILURE);
-	}
+		ft_errorexit("Error\nCharacter texture failed to load\n");
 	data->exit = mlx_xpm_file_to_image(data->mlx_ptr, "./images/exit.xpm", &data->width, &data->height);
 	if (!data->exit)
-	{
-		ft_putstr_fd("Error\nExit texture failed to load\n", 2);
-		exit(EXIT_FAILURE);
-	}
+		ft_errorexit("Error\nExit texture failed to load\n");
 }
